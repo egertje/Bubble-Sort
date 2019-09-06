1a3d5cae1d6b07ad4c73f82afd2e7930abcb0ebb
Emma Gertje <egertje@wpi.edu>
2019-09-06 10:42:27 -0400

    Added the code for the bubble sort algorithm.

---
+++ BubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+	
+	public int[] bubbleSortAlgorithm(int[] array) {
+		boolean traverseAgain = true;
+        while (traverseAgain) {
+        	traverseAgain = false;
+        	for(int i = 1; i < array.length; i++) {
+        		int temp;
+        		if (array[i-1] > array[i]) {
+        			temp = array[i-1];
+        			array[i-1] = array[i];
+        			array[i] = temp;
+        			traverseAgain = true;
+        		}
+        	}
+        }
+        return array;
+	}
+	
+	public void printArray(int[] array) {
+		for(int i=0; i < array.length; i++) {
+	        System.out.print(array[i] + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BubbleSort bubble = new BubbleSort();
+        System.out.println("How many characters would you like in the array?");
+        int n = in.nextInt();
+        int ar[] = new int[n];
+        System.out.println("Please enter all the characters in the array below: ");
+        for (int ar_i = 0; ar_i < n; ar_i++) {
+            ar[ar_i] = in.nextInt();
+        }
+        System.out.println("Here is the array unsorted: ");
+        bubble.printArray(ar);
+        System.out.printf("%n");
+        int sortedArray[] = bubble.bubbleSortAlgorithm(ar);
+        System.out.println("Here is the sorted array: ");
+        bubble.printArray(sortedArray);
+        
+	}
+}
