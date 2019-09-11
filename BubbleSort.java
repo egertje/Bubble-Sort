91c6629141aab855ac91928eb338a8771c371e61
Emma Gertje <egertje@wpi.edu>
2019-09-11 11:22:15 -0400

    Changed ordering of methods in program.

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class BubbleSort {
 	
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BubbleSort bubble = new BubbleSort();
+        System.out.println("How many characters would you like in the array?");
+        int n = in.nextInt();
+        int ar[] = new int[n];
+        System.out.println("Please enter all the characters in the array below: ");
+        for (int ar_i = 0; ar_i < n; ar_i++) {
+            ar[ar_i] = in.nextInt();
+        }
+        System.out.println("Here is the array unsorted: ");
+        bubble.printArray(ar);
+        System.out.printf("%n");
+        int sortedArray[] = bubble.bubbleSortAlgorithm(ar);
+        System.out.println("Here is the sorted array: ");
+        bubble.printArray(sortedArray);  
+	}
+	
 	public int[] bubbleSortAlgorithm(int[] array) {
 		boolean traverseAgain = true;
         while (traverseAgain) {
@@ -23,24 +41,5 @@ public class BubbleSort {
 		for(int i=0; i < array.length; i++) {
 	        System.out.print(array[i] + " ");
 		}
-	}
-	
-	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        BubbleSort bubble = new BubbleSort();
-        System.out.println("How many characters would you like in the array?");
-        int n = in.nextInt();
-        int ar[] = new int[n];
-        System.out.println("Please enter all the characters in the array below: ");
-        for (int ar_i = 0; ar_i < n; ar_i++) {
-            ar[ar_i] = in.nextInt();
-        }
-        System.out.println("Here is the array unsorted: ");
-        bubble.printArray(ar);
-        System.out.printf("%n");
-        int sortedArray[] = bubble.bubbleSortAlgorithm(ar);
-        System.out.println("Here is the sorted array: ");
-        bubble.printArray(sortedArray);
-        
 	}
 }
